ccadc44ba1046279979ca9be4dbfa18c526c4111
AlexeiM2004 <alexei.maiorov@student.manchester.ac.uk>
2026-03-18 10:17:16 +0000

    Added setters and getters to both particle and momentum classes. I now need to figure out how to implement the rule of 5, and add some text to say the destructor was called, as the assignment requests.

--- momentum.h
+++ momentum.h
@@ -18,7 +18,23 @@ public:
     Momentum(); // Default constructor
     Momentum(double p0_comp, double p1_comp, double p2_comp, double p3_comp); // Parameterised constructor
     ~Momentum(); // Destructor
-    
+// Getters for each member
+    double get_p0_component() const;
+    double get_p1_component() const;
+    double get_p2_component() const;
+    double get_p3_component() const;
+    bool is_valid() const {return valid_flag;}
+// Setters for each member
+    void set_p0_component(double p0_comp);
+    void set_p1_component(double p1_comp);
+    void set_p2_component(double p2_comp);
+    void set_p3_component(double p3_comp);
+// Display function to display momentum information
+    void display_information() const;
+// Define copy assignment 
+// Define copy constructor
+// Define move constructor
+// Define move assignment
 
 };
 
--- particle.h
+++ particle.h
@@ -11,10 +11,26 @@ private:
     std::string particle_name;
     std::string particle_type;
     Momentum* momentum; 
+    bool valid_flag;
 public:
     Particle(); // Default constructor
-    Particle(std::string part_name, std::string part_type, double part_momentum); // Parameterised constructor
+    Particle(std::string part_name, std::string part_type, double p0_comp, double p1_comp, double p2_comp, double p3_comp); // Parameterised constructor
     ~Particle(); // Destructor
+// Getters for each member
+    std::string get_particle_name() const;
+    std::string get_particle_type() const;
+    double get_p0_component() const;
+    double get_p1_component() const;
+    double get_p2_component() const;
+    double get_p3_component() const;
+    bool is_valid() const {return valid_flag;}
+// Setters for each member
+    void set_particle_name(std::string part_name);
+    void set_particle_type(std::string part_type);
+    void set_p0_component(double p0_comp);
+    void set_p1_component(double p1_comp);
+    void set_p2_component(double p2_comp);
+    void set_p3_component(double p3_comp);
 };
 
 #endif 
