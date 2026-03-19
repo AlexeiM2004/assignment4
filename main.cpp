3ebc1422a4752653ede069894a06c0da1bbdab2f
AlexeiM2004 <alexei.maiorov@student.manchester.ac.uk>
2026-03-19 19:26:15 +0000

    Began work on the main function, here I demonstrate some valid particles, and some invalid particles (wrong name, or wrong momentum) then add rhe particles to a vector. Ill move onto demonstrating, addition (overloading) of particles, dot products of particles, move constructor, copy constructor, move assignment operator and copy assignment operator. Then add a display function such that all information can be seen.

--- main.cpp
+++ main.cpp
@@ -13,17 +13,38 @@
 int main()
 {
 
-    // If an invalid input was flagged in the momentum.cpp, the program exits gracefully by returning 1
-    for(const Momentum& momentum: momenta)
-    {
-        if(momentum.is_valid() == false)
-        {
-            std::cout << "\nExiting program.";
-            return 1;
-        }
-    }
+    // Test creation of valid particle 
 
-    // If an invalid input was flagged in the source.cpp, the program exits gracefully by returning 1
+    std::cout << "\nCreate valid particles.\n";
+
+    Particle Electron("Electron", 35, 57, 97, 7);
+    Particle Muon("Muon", 33, 9, 60, 67);
+    Particle Taon("Taon", 6, 25, 95, 65);
+    Particle Photon("Photon", 22, 40, 81, 10);
+
+    // Test creation of invalid particles
+
+    std::cout << "\nCreate a particle with an invalid name.\n";
+
+    Particle Chud("Chud particle", 67, 67, 67, 67); // Valid flag will be triggered as false
+
+    std::cout << "\nCreate a particle with an invalid 4 momenta.\n";
+
+    Particle SecretChud("W", -67, 67, 67, 67); // Valid flag will be trigged as false.
+
+    // Create particle array and store particles
+
+    std::vector<Particle> particles;
+
+    particles.push_back(Electron);
+    particles.push_back(Muon);
+    particles.push_back(Taon);
+    particles.push_back(Photon);
+    particles.push_back(Chud);
+    particles.push_back(SecretChud);
+
+    // If an invalid input was flagged in the particle.cpp, program exits gracefully by returning 1
+    
     for(const Particle& particle : particles)
     {
         if(particle.is_valid() == false)
